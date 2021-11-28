a74e28ff3c18cf5352cad40150f4c1fbc5748798
Alex <soyvitaly@gmail.com>
2021-11-28 17:05:35 +0100

    Added checking for a book

--- main.py
+++ main.py
@@ -1,21 +1,32 @@
 import requests
 import os
 
-def download_books(path):
-    url = 'https://tululu.org/txt.php?id=32168'
-    links = [url for num in range(10)]
 
-    for link_number, link in enumerate(links, 1):
-        response = requests.get(link)
+def check_for_redirect(response):
+    if response.history:
+        raise requests.exceptions.HTTPError
+
+
+def download_books(path, books_quantity):
+    for book_id in range(1, books_quantity+1):
+        payload = {'id': book_id}
+        url = 'https://tululu.org/txt.php'
+        response = requests.get(url, params=payload)
         response.raise_for_status()
-        with open(f"{path}/id_{link_number}.tex", "wb") as file:
+        try:
+            check_for_redirect(response)
+        except requests.exceptions.HTTPError:
+            continue
+        with open(f"{path}/id_{book_id}.tex", "wb") as file:
             file.write(response.content)
+        book_id += 1
 
 
 def main():
+    books_quantity = 10
     path = 'books'
     os.makedirs(path, exist_ok=True)
-    download_books(path)
+    download_books(path, books_quantity)
 
 
 if __name__ == '__main__':
