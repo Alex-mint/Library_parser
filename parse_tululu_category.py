49d87988d5c1bbf8d67ce28469f6c5d679208a6a
Alex <soyvitaly@gmail.com>
2021-12-09 09:48:03 +0100

    Added more argparse parameters

--- parse_tululu_category.py
+++ parse_tululu_category.py
@@ -5,12 +5,14 @@ import json
 from bs4 import BeautifulSoup
 from urllib.parse import urljoin
 
-from main import parse_book_page, download_image, download_txt
+from parser_tools import (parse_book_page, download_image,
+                          download_txt, create_parser)
 
 
-def save_book_attributes(book_attributes):
-
-    with open('description', 'w', encoding='utf-8') as json_file:
+def save_book_attributes(book_attributes, json_path):
+    os.makedirs(json_path, exist_ok=True)
+    filename = os.path.join(json_path, 'description')
+    with open(filename, 'w', encoding='utf-8') as json_file:
         json.dump(book_attributes, json_file, ensure_ascii=False, indent=2)
 
 
@@ -19,21 +21,24 @@ def get_book_urls(page_url, library_url):
     response.raise_for_status()
     soup = BeautifulSoup(response.text, 'lxml')
     crude_book_urls = soup.select('.ow_px_td .d_book')
-    book_urls = [urljoin(library_url, url.select_one('a')['href']) for url in crude_book_urls]
-
-    print(crude_book_urls)
-    print(book_urls)
+    book_urls = [urljoin(library_url, url.select_one('a')['href']) for url in
+                 crude_book_urls]
     return book_urls
 
 
-def download_book_attributes(library_url, book_urls):
+def download_book_attributes(library_url, book_urls, books_folder,
+                             images_folder, skip_imgs, skip_txt):
     all_book_attributes = []
     for book_url in book_urls:
         try:
             book_id = book_url.split('/')[-2].replace('b', '')
             book_attributes = parse_book_page(book_url)
-            download_image(book_attributes['image_url'], library_url)
-            download_txt(library_url, book_attributes['filename'], book_id)
+            if not skip_imgs:
+                download_image(book_attributes['image_url'], library_url,
+                               images_folder)
+            if not skip_txt:
+                download_txt(library_url, book_attributes['filename'], book_id,
+                             books_folder)
         except requests.exceptions.HTTPError:
             continue
         all_book_attributes.append(book_attributes)
@@ -43,13 +48,27 @@ def download_book_attributes(library_url, book_urls):
 def main():
     library_url = 'https://tululu.org/'
     book_attributes = []
-    for page in range(1, 2):
+
+    books_parser = create_parser()
+    args = books_parser.parse_args()
+    books_folder = os.path.join(args.dest_folder, 'books')
+    images_folder = os.path.join(args.dest_folder, 'images')
+    skip_imgs = args.skip_imgs
+    skip_txt = args.skip_txt
+
+    json_path = os.path.join(args.json_path)
+    if not args.json_path:
+        json_path = os.path.join(args.dest_folder)
+
+    for page in range(args.start_page, args.end_page):
         page_url = f'{library_url}l55/{page}/'
         if page == 1:
             page_url = f'{library_url}l55/'
         book_urls = get_book_urls(page_url, library_url)
-        book_attributes += download_book_attributes(library_url, book_urls)
-    save_book_attributes(book_attributes)
+        book_attributes += download_book_attributes(library_url, book_urls,
+                                                    books_folder, images_folder,
+                                                    skip_imgs, skip_txt)
+    save_book_attributes(book_attributes, json_path)
 
 
 if __name__ == '__main__':
--- parser_tools.py
+++ parser_tools.py
@@ -8,10 +8,24 @@ from urllib.parse import urljoin
 from urllib import parse
 
 
+def get_last_page():
+    url = 'https://tululu.org/l55/'
+    response = requests.get(url)
+    check_for_redirect(response)
+    response.raise_for_status()
+    soup = BeautifulSoup(response.text, 'lxml')
+    last_page = int(soup.select_one('.npage:last-child').text)
+    return last_page
+
+
 def create_parser():
     books_parser = argparse.ArgumentParser(description='Параметры запуска скрипта')
-    books_parser.add_argument('--start_id', default=1, type=int)
-    books_parser.add_argument('--end_id', default=766, type=int)
+    books_parser.add_argument('--start_page', default=1, type=int)
+    books_parser.add_argument('--end_page', default=get_last_page(), type=int)
+    books_parser.add_argument('--dest_folder', default='downloads', type=str)
+    books_parser.add_argument('--json_path', default='', type=str)
+    books_parser.add_argument('--skip_imgs', action='store_true')
+    books_parser.add_argument('--skip_txt', action='store_true')
     return books_parser
 
 
@@ -20,19 +34,18 @@ def check_for_redirect(response):
         raise requests.exceptions.HTTPError
 
 
-def download_txt(library_url, filename, book_id, txt_folder='books/'):
-    os.makedirs(txt_folder, exist_ok=True)
+def download_txt(library_url, filename, book_id, books_folder):
+    os.makedirs(books_folder, exist_ok=True)
     payload = {'id': book_id}
     response = requests.get(f'{library_url}txt.php', params=payload)
     check_for_redirect(response)
     response.raise_for_status()
-    filename = sanitize_filename(filename)
-    with open(f"{txt_folder}/{filename}.txt", "w",
-              encoding='utf8') as file:
+    filename = os.path.join(books_folder, sanitize_filename(filename))
+    with open(f'{filename}.txt', 'w', encoding='utf8') as file:
         file.write(response.text)
 
 
-def download_image(chopped_image_url, library_url, images_folder='images/'):
+def download_image(chopped_image_url, library_url, images_folder):
     os.makedirs(images_folder, exist_ok=True)
     if chopped_image_url == 'nopic.gif':
         image_url = f'{library_url}/images/{chopped_image_url}'
@@ -41,7 +54,8 @@ def download_image(chopped_image_url, library_url, images_folder='images/'):
     response = requests.get(image_url)
     check_for_redirect(response)
     response.raise_for_status()
-    with open(f"{images_folder}/{chopped_image_url}", "wb") as file:
+    filename = os.path.join(images_folder, chopped_image_url)
+    with open(filename, "wb") as file:
         file.write(response.content)
 
 
@@ -64,22 +78,3 @@ def parse_book_page(book_url):
     }
     return book_attributes
 
-
-def main():
-    books_parser = create_parser()
-    args = books_parser.parse_args()
-    library_url = 'https://tululu.org/'
-    print('stary')
-
-    for book_id in range(args.start_id, args.end_id + 1):
-        book_url = f'{library_url}b{book_id}/'
-        try:
-            book_attributes = parse_book_page(book_url)
-            download_txt(library_url, book_attributes['filename'], book_id)
-            download_image(book_attributes['image_url'], library_url)
-        except requests.exceptions.HTTPError:
-            continue
-
-
-if __name__ == '__main__':
-    main()
